470d88c5be9aeec8aa075f75f64d4c7e472b1faf
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-01-24 11:45:46 -0800

    revamping whole site

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/nodeauth',
-  secret: 'yoursecret'
-}
+  // configure the code below with your username, password and mlab database information
+  database: "mongodb://mateen:mateen1222@ds253840.mlab.com:53840/meanauthapp", //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: "secret"
+};
+m;
